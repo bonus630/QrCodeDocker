45f8927f04d41becd1300226e740ed23c9e9be33
Reginaldo Santos <bonus630@gmail.com>
2022-04-26 16:32:27 -0300

    Add some lang tags

--- PluginLoader/LangTagsEnum.cs
+++ PluginLoader/LangTagsEnum.cs
@@ -8,12 +8,16 @@ using System.ComponentModel;
 
 namespace br.corp.bonus630.PluginLoader
 {
-    
+    //https://docs.microsoft.com/pt-br/openspecs/windows_protocols/ms-lcid/a9eac961-e77d-41a6-90a5-ce1a8b0cdb9c
     public enum LangTagsEnum
     {
         PT_BR,
         EN_US,
-        ES_ES
+        ES_ES,
+        RU_RU,
+        DE_DE,
+        FR_FR,
+        ZH_Hant
     }
     public static class LangConvertion
     {
@@ -30,6 +34,18 @@ namespace br.corp.bonus630.PluginLoader
                     break;
                 case cdrTextLanguage.cdrSpanish:
                     tag = LangTagsEnum.ES_ES;
+                    break;
+                case cdrTextLanguage.cdrTraditionalChinese:
+                    tag = LangTagsEnum.ZH_Hant;
+                    break;
+                case cdrTextLanguage.cdrRussian:
+                    tag = LangTagsEnum.RU_RU;
+                    break;
+                case cdrTextLanguage.cdrGerman:
+                    tag = LangTagsEnum.DE_DE;
+                    break;
+                case cdrTextLanguage.cdrFrench:
+                    tag = LangTagsEnum.FR_FR;
                     break;
             }
             return tag;
